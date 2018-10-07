bca12cdaeb4a50e9a28fb02baa10fb23f651a6ae
Saeed Padkan <s.padkan@gmail.com>
2018-10-07 15:35:47 +0200

    change session_start place

--- webroot/index.php
+++ webroot/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
@@ -6,7 +7,7 @@ define('VIEWS_PATH', ROOT.DS.'views');
 
 require_once(ROOT.DS.'lib'.DS.'init.php');
 
-session_start();
+
 
 App::run($_SERVER['REQUEST_URI']);
 
